1d9b53acaf7acb09840bb0153132d72aed08e016
Mredosz <mredosz4@gmail.com>
2024-10-12 18:12:43 +0200

    Reorganize and add footer

--- frontend/src/components/products/reusable/ProductInfo.jsx
+++ frontend/src/components/products/reusable/ProductInfo.jsx
@@ -2,7 +2,7 @@ export default function ProductInfo({ left, right }) {
   return (
     <div className="flex justify-between">
       <p className="font-bold">{left}</p>
-      <p>{right}</p>
+      <p className="pr-3">{right}</p>
     </div>
   );
 }
