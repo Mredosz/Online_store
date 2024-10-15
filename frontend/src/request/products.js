b591f8a7a0ddfbddf72c0871070dfa24192ff7e5
Mredosz <mredosz4@gmail.com>
2024-10-15 20:58:39 +0200

    Add more functions

--- frontend/src/request/products.js
+++ frontend/src/request/products.js
@@ -9,3 +9,35 @@ export const getProductDetails = async (id) => {
     await axios.get(`http://localhost:3000/products/details?productId=${id}`)
   ).data;
 };
+
+export const getProductFromCategory = async (id) => {
+  return (
+    await axios.get(`http://localhost:3000/products/category?categoryId=${id}`)
+  ).data;
+};
+
+export const addProduct = async (product) => {
+  return (
+    await axios.post("http://localhost:3000/products/", product, {
+      withCredentials: true,
+    })
+  ).data;
+};
+
+export const updateProduct = async (product, id) => {
+  return (
+    await axios.put(
+      `http://localhost:3000/products/?productId=${id}`,
+      product,
+      { withCredentials: true },
+    )
+  ).data;
+};
+
+export const deleteProduct = async (id) => {
+  return (
+    await axios.delete(`http://localhost:3000/products/?productId=${id}`, {
+      withCredentials: true,
+    })
+  ).data;
+};
