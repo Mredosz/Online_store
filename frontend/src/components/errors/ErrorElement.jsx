240db57be8cbd155589c8096bc1cf3eb1a4c08cf
mredo <mredosz4@gmail.com>
2024-11-21 20:03:19 +0100

    Add error page

--- frontend/src/components/errors/ErrorElement.jsx
+++ frontend/src/components/errors/ErrorElement.jsx
@@ -1,7 +1,31 @@
+import { Link, useRouteError } from "react-router-dom";
+
 export default function ErrorElement() {
+  let message = "Could not find page or resource.";
+  const error = useRouteError();
+
+  if (error.message) {
+    message = error.message;
+  }
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="flex flex-col justify-center h-screen">
+      <h1 className="text-red-600 text-4xl font-bold text-center m-4">
+        Error occurred!
+      </h1>
+      {error.status && (
+        <p className="text-center text-xl text-yellow-400">
+          Status code: {error.status}
+        </p>
+      )}
+      <p className="text-yellow-400 text-center text-xl m-4">{message}</p>
+      <div className="text-center">
+        <Link
+          to="/"
+          className="bg-amber-200 rounded p-1 text-center hover:bg-amber-300"
+        >
+          Return to the main page
+        </Link>
+      </div>
     </div>
   );
 }
