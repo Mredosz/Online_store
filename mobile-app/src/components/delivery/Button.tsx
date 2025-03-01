2dd442e4534f9f3eee740302709c8fdae0a92052
Mredosz <mredosz4@gmail.com>
2025-03-01 18:27:42 +0100

    Add props

--- mobile-app/src/components/delivery/Button.tsx
+++ mobile-app/src/components/delivery/Button.tsx
@@ -1,6 +1,17 @@
-import { Pressable, Text } from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
+import { ReactNode } from "react";
 
-export default function Button({ children, className, isValid, ...props }) {
+type ButtonProps = PressableProps & {
+  children: ReactNode;
+  isValid: boolean;
+};
+
+export default function Button({
+  children,
+  className,
+  isValid,
+  ...props
+}: ButtonProps) {
   const classes = `py-2 px-4 mt-4 rounded-md bg-green-700 ${isValid && "active:bg-green-900"} ${className}`;
 
   return (
